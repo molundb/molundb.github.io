21569c959365939a817ab6e22099c824c1ce3b2b
molundb <martin_o_lundberg@hotmail.com>
2022-12-16 23:42:54 +0100

    refactor/remove unused footer file

--- footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '<ul>
-      <li>martin_o_lundberg at hotmail.com</li>
-      <li><a href="https://github.com/molundb">github.com/molundb</a></li>
-      <li>
-        <a href="https://www.linkedin.com/in/martin-lundberg/">LinkedIn</a>
-      </li>
-    </ul>'
